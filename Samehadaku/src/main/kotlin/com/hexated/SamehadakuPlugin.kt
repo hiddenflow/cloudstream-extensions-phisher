3bf09cd314183ef0820095b87ed205a5b2ee1f80
hiddenflow <69741367+hiddenflow@users.noreply.github.com>
2025-04-28 19:15:36 +0700

    Update SamehadakuPlugin.kt

--- Samehadaku/src/main/kotlin/com/hexated/SamehadakuPlugin.kt
+++ Samehadaku/src/main/kotlin/com/hexated/SamehadakuPlugin.kt
@@ -1,6 +1,8 @@
 
 package com.hexated
 
+import com.lagradost.cloudstream3.extractors.Gofile
+import com.lagradost.cloudstream3.extractors.Krakenfiles
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
@@ -10,5 +12,8 @@ class SamehadakuPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Samehadaku())
+
+        registerExtractorAPI(Gofile())
+        registerExtractorAPI(Krakenfiles())
     }
 }
